2a7eb0b41232a20b319064d05a3feb5b266426a6
MonodetH <monodeth@gmail.com>
2012-08-04 15:47:11 -0400

    Agregada Funcionalidad SUMAR

--- class_core.py
+++ class_core.py
@@ -1,7 +1,8 @@
 from PyQt4 import QtGui, QtCore, uic
+from types import *
 import sys
 
-class TableModel(QtCore.QAbstractTableModel):
+class EvalTableModel(QtCore.QAbstractTableModel):
 	
 	def __init__(self, datos = [[]], parent = None):
 		QtCore.QAbstractTableModel.__init__(self, parent)
@@ -34,13 +35,10 @@ class TableModel(QtCore.QAbstractTableModel):
 			row = index.row()
 			column = index.column()
 			color = value
-			self.__datos[row][column] = color
+			self.__datos[row][column] = color.toString()
 			self.dataChanged.emit(index, index)
 			return True
 		return False
-
-
-
 
 	def headerData(self, section, orientation, role):
 		if role == QtCore.Qt.DisplayRole:
@@ -52,14 +50,105 @@ class TableModel(QtCore.QAbstractTableModel):
 			else:
 				return QtCore.QString("Eval %1").arg(section+1)
 
+	def getNota(self, nombre):
+		for i in range(self.rowCount()):
+			row = self.__datos[i]
+			if row[0] == nombre:
+				return int(row[1])
+		return 0
 
 
     #=====================================================#
     #INSERTING & REMOVING
     #=====================================================#
-	def insertRow(self, position, nombre, nota, parent = QtCore.QModelIndex()):
+	def insertRow(self, position, values, parent = QtCore.QModelIndex()):
 		self.beginInsertRows(parent, position, position)
-		values = [nombre,nota]
+		self.__datos.insert(position, values)
+		self.endInsertRows()
+		return True
+
+	def removeRow(self, position, parent = QtCore.QModelIndex()):
+		self.beginRemoveRows(parent, position, position)
+		values = self.__datos[position]
+		self.__datos.remove(values)
+		self.endRemoveRows()
+		return True
+
+class VarTableModel(QtCore.QAbstractTableModel):
+	
+	def __init__(self, datos = [[]], parent = None):
+		QtCore.QAbstractTableModel.__init__(self, parent)
+		self.__datos = datos
+		self.__headers = ["Nombre", "Valor", "Tipo", "Parametros", "Ponderacion"]
+
+	def rowCount(self, parent = None):
+		return len(self.__datos)
+
+	def columnCount(self, parent):
+		return len(self.__datos[0])
+
+	def flags(self, index):
+		return QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+
+	def data(self, index, role):
+		if role == QtCore.Qt.EditRole:
+			row = index.row()
+			column = index.column()
+			return self.__datos[row][column]
+
+		if role == QtCore.Qt.DisplayRole:
+			row = index.row()
+			column = index.column()
+			value = self.__datos[row][column]
+			return value
+
+	def setData(self, index, value, role = QtCore.Qt.EditRole):
+		if role == QtCore.Qt.EditRole:
+			row = index.row()
+			column = index.column()
+			color = value
+			self.__datos[row][column] = color.toString()
+			self.dataChanged.emit(index, index)
+			return True
+		return False
+
+	def setValor(self, row, value, role = QtCore.Qt.EditRole):
+		if role == QtCore.Qt.EditRole:
+			color = value
+			self.__datos[row][1] = str(color)
+			return True
+		return False
+	
+	def headerData(self, section, orientation, role):
+		if role == QtCore.Qt.DisplayRole:
+			if orientation == QtCore.Qt.Horizontal:
+				if section < len(self.__headers):
+					return self.__headers[section]
+				else:
+					return "not implemented"
+			else:
+				return QtCore.QString("Var %1").arg(section+1)
+	
+	def getVar(self, nombre):
+		for i in range(self.rowCount()):
+			row = self.__datos[i]
+			if row[0] == nombre:
+				return [row[2],row[3],row[4]]
+		return 0
+	
+	def ubicaNombre(self, nombre):
+		for i in range(self.rowCount()):
+			row = self.__datos[i]
+			if row[0] == nombre:
+				return i
+		return None
+		
+		
+    #=====================================================#
+    #INSERTING & REMOVING
+    #=====================================================#
+	def insertRow(self, position, values, parent = QtCore.QModelIndex()):
+		self.beginInsertRows(parent, position, position)
 		self.__datos.insert(position, values)
 		self.endInsertRows()
 		return True
--- class_mainwindow.py
+++ class_mainwindow.py
@@ -22,19 +22,38 @@ class Principal(QtGui.QMainWindow):
 		self.ventana = Ui_MainWindow()
 		self.ventana.setupUi(self)
         
-		self.model1 = TableModel([["EDITAME","0"]])
+		self.model1 = EvalTableModel([["EDITAME","0"]])
 		self.ventana.tableView.setModel(self.model1)
-        
+
+		self.model2 = VarTableModel([["NF","0","SUMA","EDITAME","1.0"]])
+		self.ventana.tableView_2.setModel(self.model2)
+
 		self.ventana.pushButton.connect(self.ventana.pushButton, SIGNAL("clicked()"),self, SLOT("addEval()"))
 		self.ventana.pushButton_2.connect(self.ventana.pushButton_2, SIGNAL("clicked()"),self, SLOT("addVar()"))
-		self.ventana.pushButton_3.connect(self.ventana.pushButton_3, SIGNAL("clicked()"),self, SLOT("reset()"))
+		self.ventana.pushButton_3.connect(self.ventana.pushButton_3, SIGNAL("clicked()"),self, SLOT("resetAll()"))
+		self.ventana.pushButton_4.connect(self.ventana.pushButton_4, SIGNAL("clicked()"),self, SLOT("generarNF()"))
 		
+	def generar(self,nombre):
+		row = self.model2.getVar(nombre)
+		if row == 0:
+			return self.model1.getNota(nombre)
+		elif row[0] == "SUMA":
+			param = [x.strip() for x in str(row[1]).split(',')]
+			retorno = 0
+			for x in param:
+				retorno += self.generar(x)
+			nota = float(row[2])*retorno
+			ubica = self.model2.ubicaNombre(nombre)
+			self.model2.setValor(ubica, nota)
+			return nota
+			
+	
 	@pyqtSlot()
 	def addEval(self):
 		self.popEval = PopEval()
 		r = self.popEval.exec_()
 		if r:
-			self.model1.insertRow(self.model1.rowCount(),self.popEval.ventana.lineEdit.text(),str(self.popEval.ventana.spinBox.value()))
+			self.model1.insertRow(self.model1.rowCount(),[self.popEval.ventana.lineEdit.text(),str(self.popEval.ventana.spinBox.value())])
 		
 	@pyqtSlot()
 	def addVar(self):
@@ -43,6 +62,21 @@ class Principal(QtGui.QMainWindow):
 		print r
 		
 	@pyqtSlot()
-	def reset(self):
-		self.model1 = TableModel([["EDITAME","0"]])
+	def resetAll(self):
+		self.model1 = EvalTableModel([["EDITAME","0"]])
 		self.ventana.tableView.setModel(self.model1)
+		
+		self.model2 = VarTableModel([["NF","0","SUMA","EDITAME","1.0"]])
+		self.ventana.tableView_2.setModel(self.model2)
+		
+	@pyqtSlot()
+	def generarNF(self):
+		self.generar("NF")
+		self.ventana.tableView_2.setModel(self.model2)
+	
+
+
+
+
+
+		

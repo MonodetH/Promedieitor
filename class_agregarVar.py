5c689c15d646e2a7118783988e32e247caad2d5b
nikopain <nicolas.curapil@alumnos.usm.cl>
2012-08-04 19:47:16 -0400

    Modificado UI y Class de agregarVar
    Implementado El comboBox

--- class_agregarVar.py
+++ class_agregarVar.py
@@ -18,7 +18,22 @@ class PopVar(QtGui.QDialog):
         # Se crea la instancia de Ui_Dialog
         self.ventana = Ui_AgreVar()
         self.ventana.setupUi(self)
-        
+
         self.ventana.buttonBox.connect(self.ventana.buttonBox, SIGNAL("accepted()"),self, SLOT("accept()"))
         self.ventana.buttonBox.connect(self.ventana.buttonBox, SIGNAL("rejected()"),self, SLOT("reject()"))
+        self.ventana.comboBox.connect(self.ventana.comboBox,SIGNAL("currentIndexChanged(QString)"),self,SLOT("deshabil"))
+        self.ventana.comboBox.currentIndexChanged[str].connect(self.deshabil)
 
+    def deshabil(self):
+        tipo=self.ventana.comboBox.currentText()
+        print tipo
+        if tipo == "BASE":
+            self.ventana.parVar.setEnabled(0)
+            self.ventana.pondVar.setEnabled(0)
+            self.ventana.label_3.setEnabled(0)
+            self.ventana.label_7.setEnabled(0)
+        else:
+            self.ventana.parVar.setEnabled(1)
+            self.ventana.pondVar.setEnabled(1)
+            self.ventana.label_3.setEnabled(1)
+            self.ventana.label_7.setEnabled(1)
